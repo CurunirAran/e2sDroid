c38adae0502ec785d015428a6073570b82fc49de
Pratyush Mishra <pratyushmishra@berkeley.edu>
2013-06-13 14:29:43 +0300

    Changed layout, added documentation.

--- src/com/arcurunir/e2s/MainActivity.java
+++ src/com/arcurunir/e2s/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arcurunir.e2s;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.text.SpannedString;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,20 +36,48 @@ public class MainActivity extends Activity {
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
-    ((EditText) findViewById(R.id.inputText)).setOnEditorActionListener(new OnEditorActionListener() {
-      @Override
-      public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        if (event != null) {
-          InputMethodManager imm = 
-              (InputMethodManager) getBaseContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-           imm.hideSoftInputFromWindow(((EditText) findViewById(R.id.inputText)).getWindowToken(), 0);
-          lookupAndOutput();
-          return true;
+    
+    // Initialize the EditText inputText and the SQLite Database.
+    inputTextInit();
+    sqlDBInit();
+    
+    return true;
+  }
+  
+  /**
+   * Initialize the inputText field.
+   */
+  
+  private void inputTextInit() {
+    EditText inputText = (EditText) findViewById(R.id.inputText);
+    inputText.setOnEditorActionListener(inputTextALInit());
+  }
+  
+  /**
+   * Initialize the OneEditorActionListener for inputText.
+   * @return the initialized OnEditorActionListener
+   */
+  private OnEditorActionListener inputTextALInit() {
+    
+    OnEditorActionListener returnValue = 
+      new OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+          if (actionId == EditorInfo.IME_ACTION_GO) {          
+            lookupAndOutput();
+            return true;
+          }
+          return false;
         }
-        return false;
-      }
-    });
-    sqlDB = new DataBaseHelper(this);
+      };
+    return returnValue;
+  }
+  
+  /**
+   * Initialize the SQLite Database.
+   */
+  private void sqlDBInit() {
+    this.sqlDB = new DataBaseHelper(this);
     try {
       sqlDB.createDataBase();
       sqlDB.openDataBase();
@@ -56,45 +86,74 @@ public class MainActivity extends Activity {
     } catch (SQLException sqle) {
       System.out.println(sqle);
     }
-    return true;
   }
   
-  
+  /**
+   * Set the action done when enterButton is pressed.
+   * @param v is the View to which the button belongs.
+   */
   public void enterClick(View v) {
     lookupAndOutput();
-    InputMethodManager imm = 
-        (InputMethodManager) getBaseContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-     imm.hideSoftInputFromWindow(((EditText) findViewById(R.id.inputText)).getWindowToken(), 0);
   }
   
-  public void lookupAndOutput() {
+  /**
+   * lookupAndOutput() is responsible for looking up the input. Generates error toasts when 
+   * the input is invalid or if there is no translation for the input.
+   * Otherwise, it appends the output to the TextView translations. 
+   */
+  private void lookupAndOutput() {
+    
+    // Hides keyboard upon querying.
+    InputMethodManager imm = (InputMethodManager) getBaseContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+    imm.hideSoftInputFromWindow(((EditText) findViewById(R.id.inputText)).getWindowToken(), 0);
+    
+    // Finds the required elements.
     EditText enterText = (EditText) findViewById(R.id.inputText);
     TextView translations = (TextView) findViewById(R.id.translations);
-    String input = enterText.getText().toString();
+    
+    String input = enterText.getText().toString().toLowerCase(Locale.UK);
+    
     if (enterText.getText().length() == 0) {
+      // If the input is invalid, generate an error Toast.
       errorToast("Please enter a valid word");
     } else {
       try {
         Cursor queryResult = sqlDB.executeQry("select * from ensd where word = ?", input);
         if (!queryResult.moveToNext()) {
+          
+          // If there exists no translation for the input, generate an error Toast.
           errorToast("Word not found in dictionary. Sorry!");
         } else {
+          
           translations.setText(new SpannedString(""));
+          // Iteratively look up the various fields of the result and append them 
+          // to the TextView 'translations'.
           do {
             String translation = queryResult.getString(queryResult.getColumnIndex("translation"));
             String pos = queryResult.getString(queryResult.getColumnIndex("pos"));
             String tense = queryResult.getString(queryResult.getColumnIndex("tense"));
             String usage = queryResult.getString(queryResult.getColumnIndex("usage"));
-            output(translations, translation, pos, tense, usage);
+            
+            appendResult(translations, translation, pos, tense, usage);
           } while(queryResult.moveToNext());
         }
-      } catch(Exception e) {
-        System.err.println(e);
+      } catch(SQLException sqle) {
+        System.err.println(sqle);
       }
     }
   }
   
-  void output(TextView translations, String tr, String p, String te, String u) {
+  /**
+   * appendResult() appends the various linguistic data to the 'translations' TextView.
+   * @param translations the TextView to which the data is to be appended.
+   * @param tr the Sindarin translation of the input.
+   * @param p the Part of Speech to which the translation belongs.
+   * @param te the tense of the translation.
+   * @param u the usage of the translation
+   */
+  
+  private void appendResult(TextView translations, String tr, String p, String te, String u) {
+    
     translations.append(Html.fromHtml("Sindarin translation: <b>" + tr + "</b><br>\n"));
     translations.append("Part of Speech: " + p + "\n");
     if (!te.equals("-")) {
@@ -106,7 +165,11 @@ public class MainActivity extends Activity {
     translations.append("\n");
   }
   
-  public void errorToast(String message) {
+  /**
+   * errorToast() generates a Toast containing text passed as a parameter.
+   * @param message the text to be displayed.
+   */
+  private void errorToast(String message) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show();
   }
 
